a5d4ac7ad1c387220194f557dd892fa32377e000
Paul Saieg <classicist@gmail.com>
2010-11-10 23:35:23 -0700

    aded process forking

--- lib/fishes/fish.rb
+++ lib/fishes/fish.rb
@@ -1,11 +1,23 @@
 class Fish
-  attr_reader :pid, :compiler_arguments
-
+  attr_reader :pid, :compiler_arguments, :exit_status
+  TERM = "TERM"
+  
   def initialize(compiler_arguments)
-    @pid = "3"
     @compiler_arguments = compiler_arguments
   end
     
+  def start
+    @pid = fork do
+
+    end
+  end
+  
+  def stop
+    @exit_status = Process.kill TERM, pid if pid
+    @pid = nil
+    @exit_status
+  end
+  
   def ==(other)
     return false if other.nil?
     other.hash == hash
--- spec/fishes_spec.rb
+++ spec/fishes_spec.rb
@@ -10,6 +10,7 @@ describe "Place your specs here" do
   end
   
   after(:each) do
+    @fish.stop();
     @fish = nil
   end
   
@@ -18,7 +19,7 @@ describe "Place your specs here" do
   end
   
   it "should hold a process id" do
-    @fish.pid.should_not be_nil
+    @fish.pid.should be_nil
   end
   
   it "should be equal by its arguments" do
@@ -32,6 +33,12 @@ describe "Place your specs here" do
   end
   
   it "should fork a process" do
-    
+    @fish.start.should_not be_nil
+  end
+  
+  it "should stop a forked process" do
+    @fish.start
+    @fish.stop.should == 1
+    @fish.pid.should be_nil
   end
 end
