5e5b4d235ab77fb9540e3dbbc997645573aa7c2f
psaieg <psaieg@clickfox.com>
2010-12-07 19:15:02 -0700

    made config files generic for s/r

--- conf/localize_config.rb
+++ conf/localize_config.rb
@@ -2,13 +2,11 @@ $:.unshift(File.dirname(__FILE__))
 require 'local_constants'
 
 files = Dir.new("./xml").entries.map{ |filename| filename unless filename =~ /^\..*/}.compact
-PAULS_FLEX_HOME               = "/Users/monster/bin/flex_builder_sdk"
-PAULS_REPOSITORY_BASE_DIR     = "/Users/monster/Development/repos/Current"
 
 files.each do |file_name|
   
   file = File.open("./xml/" + file_name, "r")
-  repository_base_replacements =  file.read.gsub(/#{PAULS_REPOSITORY_BASE_DIR}/, "REPOSITORY_BASE_DIR")
+  repository_base_replacements =  file.read.gsub("REPOSITORY_BASE_DIR", "#{REPOSITORY_BASE_DIR}")
   file.close
   
   if repository_base_replacements
@@ -18,7 +16,7 @@ files.each do |file_name|
   end
   
   file = File.open("./xml/" + file_name, "r")
-  flex_home_replacements =  file.read.gsub(/#{PAULS_FLEX_HOME}/, "FLEX_HOME")
+  flex_home_replacements =  file.read.gsub("FLEX_HOME", "#{FLEX_HOME}")
   file.close
    
   if flex_home_replacements
