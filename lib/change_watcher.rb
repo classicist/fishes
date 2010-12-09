48e60fd2798f6b3be5f374450a94e2c722a8c696
Paul Saieg <classicist@gmail.com>
2010-12-09 02:03:13 -0700

    added dependaent compiles to file watching

--- lib/change_watcher.rb
+++ lib/change_watcher.rb
@@ -2,15 +2,14 @@
 #http://paulhorman.com/filesystemwatcher/
 $:.unshift(File.dirname(__FILE__))
 require 'rubygems'
-require "filesystemwatcher"
+require "watcher/filesystemwatcher"
 require "bin/fishing_pole"
 require '../conf/constants'
-require 'sinatra'
 
 class ChangeWatcher
   attr_accessor :watcher, :started
   
-  def initialize(dir_to_watch, file_name_match_to_watch = "*", delay = 1)
+  def initialize(dir_to_watch, file_name_match_to_watch = "*", delay = 3)
     @watcher = FileSystemWatcher.new(dir_to_watch, file_name_match_to_watch)
     watcher.addDirectory(dir_to_watch, file_name_match_to_watch)
     watcher.sleepTime = delay
@@ -28,19 +27,38 @@ end#EOC
 
 
 class ChangeWatcherPool
-  @@watchers = {}
+  @watchers = {}
 
   def get(dir_to_watch, file_name_match_to_watch = "*")
-    @@watchers[dir_to_watch + file_name_match_to_watch] ||= ChangeWatcher.new(dir_to_watch, file_name_match_to_watch)
+    @watchers[dir_to_watch + file_name_match_to_watch] ||= ChangeWatcher.new(dir_to_watch, file_name_match_to_watch)
   end
 end
 
 class ChangeRunner
   def self.start
-    puts "started watching #{ASSETS_BIN}"
-    @@change_watcher = ChangeWatcherPool.new
-    @@watcher = @@change_watcher.get(ASSETS_BIN, "*.swc")
-    @@watcher.start "copy_assets" 
+    @@change_watcher = ChangeWatcherPool.new    
+    recompile_management_after_dependant_compile
+    recompile_cea_after_dependant_compile
+    copy_assests_after_compile_and_recompile_login
+  end
+
+private
+  def self.copy_assests_after_compile_and_recompile_login
+    recompile ASSETS_BIN, "*.swc", ["copy_assets", "login"]             
+  end
+  
+  def self.recompile_cea_after_dependant_compile
+    recompile CEA_WEB_CONTENT, "/(assets)|(LoginModule)\.swf/", ["cea", "cea_test","cea_spike"]         
+  end
+
+  def self.recompile_management_after_dependant_compile   
+    recompile MANAGEMENT_WEB_CONTENT, "/(assets)|(LoginModule)\.swf/",["management"]   
+  end
+  
+  def self.recompile(dir, file_pattern, call_backs)
+    puts "started watching #{dir}"
+    watcher = @@change_watcher.get(dir, file_pattern)
+    call_backs.each { |call_back|  watcher.start call_back )
   end
 end
 
