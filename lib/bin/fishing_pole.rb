9da410fdfc57ea815e5146fca8ed9f347b9a7911
psaieg <psaieg@clickfox.com>
2010-12-09 10:14:26 -0700

    made the port numbers make sense

--- lib/bin/fishing_pole.rb
+++ lib/bin/fishing_pole.rb
@@ -5,35 +5,35 @@ class FishingPole
   BASE = "http://localhost"
   
   def self.cea
-    cast("4565", this_method)
+    cast("4560", this_method)
   end
 
   def self.cea_test
-    cast("5657", this_method)
+    cast("4562", this_method)
   end
   
   def self.stop
-    cast("4565", this_method)
+    cast("4560", this_method)
   end
   
   def self.login
-    cast("6789", this_method)
+    cast("4563", this_method)
   end
   
   def self.assets
-    cast("6789", this_method)
+    cast("4563", this_method)
   end
   
   def self.copy_assets
-    cast("6789", this_method)
+    cast("4563", this_method)
   end
   
   def self.cea_spike
-    cast("6789", this_method)
+    cast("4563", this_method)
   end
   
   def self.management
-    cast("7890", this_method)
+    cast("4561", this_method)
   end  
   
   def self.cast(port, url)
